f19f28053a8201670d56750f564c4c2b25b40585
EwanThomson <67035332+EwanThomson@users.noreply.github.com>
2020-06-23 19:35:38 -0700

    make winston move

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-var ewan_is_cool= true;
-alert("ALERT is cool = true")
-
-
-
+"use strict";
+var wtop=100;
+var winston=document.querySelector("#winston");
+winston.addEventListener("click",()=>{
+    wtop+=100
+    winston.style.top=wtop+"px";
+})
 
 
 
