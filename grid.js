9885abfbc0f4f746453db97bfc079bf05dc11073
EwanThomson <67035332+EwanThomson@users.noreply.github.com>
2020-07-07 20:19:34 -0700

    rainbow moving left

--- grid.js
+++ grid.js
@@ -1,3 +1,4 @@
+
 "use strict";
 var grid =document.querySelector("#grid");
 for (let row = 0; row < 8; row++) {
@@ -15,3 +16,20 @@ for (let row = 0; row < 8; row++) {
         grid.append(div)
     }
 }
+/* var img=document.createElement("img");
+img.src="rainbow-square.png"
+grid.append(img)*/
+var rainbow=63;
+grid.children[rainbow].classList.add("rainbow")
+window.addEventListener("keydown",(e)=>{
+    var newRainbow = null; 
+    if(e.key=="ArrowLeft"){
+        newRainbow=rainbow-1
+    }
+
+    if (newRainbow!=null){
+        grid.children[rainbow].classList.remove("rainbow")
+        rainbow=newRainbow
+        grid.children[rainbow].classList.add("rainbow")
+    }
+})
