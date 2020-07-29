f4178d54a2400faff2acc102c57b9e355bb5b8f0
EwanThomson <67035332+EwanThomson@users.noreply.github.com>
2020-07-29 15:00:09 -0700

    how to flip a card.

--- memory.js
+++ memory.js
@@ -2,34 +2,15 @@
 var width = 4;
 var height = 3;
 var grid =document.querySelector("#grid");
-for (let row = 0; row < height*width; row++) {
-
+var cards = []
+var revealed = ["hotdog","hotdog"]
+for (let i = 0; i < height*width; i++) {
     var div=document.createElement("div");
     div.classList.add("cell")
-    div.classList.add("blue")
+    div.dataset.reveal=revealed[i]
     grid.append(div)
-    
+    cards.push(div)
+    div.addEventListener("click",(e)=>{
+        e.target.style.backgroundImage="url(memory/"+e.target.dataset.reveal+".png)"
+    })
 }
-
-
-
-var rainbow=79;
-grid.children[rainbow].classList.add("rainbow")
-window.addEventListener("keydown",(e)=>{
-    var newRainbow = null; 
-    if(e.key=="ArrowLeft"){
-        newRainbow=rainbow-1
-    }else if(e.key=="ArrowRight"){
-        newRainbow=rainbow+1
-    }else if(e.key=="ArrowUp"){
-        newRainbow=rainbow-width
-    }else if(e.key=="ArrowDown"){
-        newRainbow=rainbow+width
-    }
-
-    if (newRainbow!=null){
-        grid.children[rainbow].classList.remove("rainbow")
-        rainbow=newRainbow
-        grid.children[rainbow].classList.add("rainbow")
-    }
-})
